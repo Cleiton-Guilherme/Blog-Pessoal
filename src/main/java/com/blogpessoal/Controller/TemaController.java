000135897ed0d2366f4d9b27674c0b846bafe5b6
Cleiton-Guilherme <Cleiton.guilherme@yahoo.com.br>
2022-08-15 18:04:24 -0300

    first commit

--- src/main/java/com/blogpessoal/Controller/TemaController.java
+++ src/main/java/com/blogpessoal/Controller/TemaController.java
@@ -1,13 +1,14 @@
 package com.blogpessoal.Controller;
 
+
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,65 +18,61 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.blogpessoal.Repository.TemaRepository;
 import com.blogpessoal.model.tema;
 
 @RestController
-@RequestMapping("/Tema")
-@CrossOrigin(origins = "*" , allowedHeaders = "*")
-public abstract class TemaController {
-	
+@RequestMapping("/temas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TemaController {
+
+	@Autowired
 	private TemaRepository temaRepository;
-	
-	public ResponseEntity<List<tema>>getAll(){
+
+	@GetMapping
+	public ResponseEntity<List<tema>> getAll() {
 		return ResponseEntity.ok(temaRepository.findAll());
-	} 
-		
-		//SELECT * FROM tb_postagens;
-		
+
+	}
+
 	@GetMapping("/{id}")
 	public ResponseEntity<tema> getById(@PathVariable Long id) {
-		return temaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+		return temaRepository.findById(id)
+			.map(resposta -> ResponseEntity.ok(resposta))
 			.orElse(ResponseEntity.notFound().build());
 	}
 
-		
-		    @GetMapping("/descrição/{descrição}")	
-		    public ResponseEntity<List<tema>>	getByDescricao(@PathVariable String descricao){
-			   return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
-		    }
-			
-			@PostMapping
-			public ResponseEntity<tema> post(@Validated @RequestBody tema tema){
-				return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
-			}
-		
-			
-			@PutMapping
-			public ResponseEntity<tema> putTema(@Valid @RequestBody tema tema) {
-					
-				return temaRepository.findById(tema.getId())
-						.map(resposta -> {
-							return ResponseEntity.ok().body(temaRepository.save(tema));
-						})
-						.orElse(ResponseEntity.notFound().build());
-			}
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<tema>> getByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
 
-	
-			@DeleteMapping("/{id}")
-			public void delete(@PathVariable Long id) {
-			     Optional<tema> postagem = temaRepository.findById(id);
-					
-				if(postagem.isEmpty())
-					throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-					
-				temaRepository.deleteById(id);
-				
-				
-				}
-						
-  }
-	
+	@PostMapping
+	public ResponseEntity<tema> postTema(@Valid @RequestBody tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
+	}
 
+	@PutMapping
+	public ResponseEntity<tema> putTema(@Valid @RequestBody tema tema) {
+					
+		return temaRepository.findById(tema.getId())
+				.map(resposta -> {
+					return ResponseEntity.ok().body(temaRepository.save(tema));
+				})
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+		
+		return temaRepository.findById(id)
+				.map(resposta -> {
+					temaRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+}
